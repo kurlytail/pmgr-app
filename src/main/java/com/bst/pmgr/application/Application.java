e09bd022822281d852f70430c4f2c9026de199a6
ashwin <ashwinkn@hotmail.com>
2018-08-14 15:41:07 -0500

    Removed default rs paths

--- src/main/java/com/bst/pmgr/application/Application.java
+++ src/main/java/com/bst/pmgr/application/Application.java
@@ -1,13 +1,16 @@
 package com.bst.pmgr.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.bst")
 @EntityScan("com.bst.pmgr.entities")
+@EnableAutoConfiguration(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
