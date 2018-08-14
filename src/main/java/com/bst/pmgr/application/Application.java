89d0f5aeb697dd39fdef00232d3f794556424773
ashwin <ashwinkn@hotmail.com>
2018-08-14 15:06:30 -0500

    Working odata and jpa integration

--- src/main/java/com/bst/pmgr/application/Application.java
+++ src/main/java/com/bst/pmgr/application/Application.java
@@ -2,10 +2,12 @@ package com.bst.pmgr.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.bst")
+@EntityScan("com.bst.pmgr.entities")
 public class Application {
 
     public static void main(String[] args) {
