ef64c6bccf810040d760878d24d04fc78d4c375a
ashwin <ashwinkn@hotmail.com>
2018-08-15 00:31:13 -0500

    Integrated pmgr-user

--- src/main/java/com/bst/pmgr/application/Application.java
+++ src/main/java/com/bst/pmgr/application/Application.java
@@ -5,15 +5,22 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
 @ComponentScan("com.bst")
-@EntityScan("com.bst.pmgr.entities")
-@EnableAutoConfiguration(exclude = RepositoryRestMvcAutoConfiguration.class)
+@EntityScan({"com.bst.pmgr.entities", "com.bst.persistence.model"})
+//@EnableAutoConfiguration(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
     }
 }
