162c9f577fdca187c494881f619b110690995b0c
ashwin <ashwinkn@hotmail.com>
2018-08-15 22:34:21 -0500

    Added relationships

--- src/main/java/com/bst/pmgr/application/Application.java
+++ src/main/java/com/bst/pmgr/application/Application.java
@@ -1,9 +1,7 @@
 package com.bst.pmgr.application;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +9,7 @@ import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
 @ComponentScan("com.bst")
-@EntityScan({"com.bst.pmgr.entities", "com.bst.persistence.model"})
+@EntityScan({"com.bst"})
 //@EnableAutoConfiguration(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class Application {
 
