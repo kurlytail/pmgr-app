7d38b1d01d972ec708c8b74bb891375b8e7daf36
ashwin <ashwinkn@hotmail.com>
2018-08-19 15:06:19 -0500

    Tomcat deployable

--- src/main/java/com/bst/pmgr/application/Application.java
+++ src/main/java/com/bst/pmgr/application/Application.java
@@ -2,11 +2,12 @@ package com.bst.pmgr.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.bst.configuration")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
