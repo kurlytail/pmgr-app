5dba35105764922dda768a9921e99baa6ed65148
ashwin <ashwinkn@hotmail.com>
2018-08-27 14:39:03 -0500

    : Implement entering new password and started tests

--- src/test/java/com/bst/pmgr/application/ApplicationTest.java
+++ src/test/java/com/bst/pmgr/application/ApplicationTest.java
@@ -1,5 +1,34 @@
 package com.bst.pmgr.application;
 
-class ApplicationTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.bst.user.authentication.components.UserController;
+import com.bst.user.registration.components.RegistrationController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ComponentScan("com.bst.configuration")
+@EnableTransactionManagement
+public class ApplicationTest {
+	
+	@Autowired
+	private RegistrationController registrationController;
+	
+	@Autowired
+	private UserController userController;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(registrationController).isNotNull();
+        assertThat(userController).isNotNull();
+    }
 
 }
