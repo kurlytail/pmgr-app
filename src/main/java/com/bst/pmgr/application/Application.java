90688c2b5cd4eb5d9cc7384c6c2a69cc37f3e5ac
ashwin <ashwinkn@hotmail.com>
2018-09-26 11:28:16 -0500

    Update

--- src/main/java/com/bst/pmgr/application/Application.java
+++ src/main/java/com/bst/pmgr/application/Application.java
@@ -1,13 +1,8 @@
 package com.bst.pmgr.application;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
